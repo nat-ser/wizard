a11fe076595defeea017a9bfeab141b7ac88a948
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2018-01-29 14:15:57 -0500

    add annotate gem;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  first_name       :string
+#  last_name        :string
+#  email            :string
+#  age              :integer
+#  height_in_inches :integer
+#  weight_in_lb     :integer
+#  fave_color       :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class User < ApplicationRecord
 end
