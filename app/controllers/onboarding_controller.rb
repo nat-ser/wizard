8b5e79f5beb330ffd0208c5ce1a1261175cdc934
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2018-01-30 20:14:40 -0500

    working on turns logic - turns not incrementing properly still

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class OnboardingController < ApplicationController
+  before_action :user, :step_count
+
   def step1
-    @user = User.new(session[:user])
   end
 
   def step2
-    @user = User.new(session[:user])
   end
 
   def step3
@@ -20,15 +20,41 @@ class OnboardingController < ApplicationController
 
   def validate_step
     @user = User.new(user_params)
-    if @user.valid?(:step1)
+    if @user.valid?(current_step)
       session[:user] = user_params
-      redirect_to onboarding_step2_url
+      @step_count += 1
+      redirect_to action: current_step
     else
-      render :step1
+      render current_step
     end
   end
 
   private
+  def user
+    @user = User.new(session[:user])
+  end
+
+  def step_count
+    @step_count ||= 1
+  end
+
+  # def increment_current_step
+  #   binding.pry
+  #   @step_count += 1
+  # end
+
+  def current_step
+    case step_count
+      when 1
+        :step1
+      when 2
+        :step2
+      when 3
+        :step3
+      when 4
+        :step4
+    end
+  end
 
   def user_params
     params.require(:user).permit(:last_name, :first_name, :email)
