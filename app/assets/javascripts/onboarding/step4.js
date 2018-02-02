0ca63e37e9d5634a92a46bcbdc94a122b28335dc
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2018-02-02 16:26:54 -0500

    fix enter custom color html

--- app/assets/javascripts/onboarding/step4.js
+++ app/assets/javascripts/onboarding/step4.js
@@ -1,17 +1,23 @@
 $(function() {
-  // listen for when user detects other user[fave_color]
+  // listen for when user select 'other' in dropdown
   function bindFaveColorOtherSelection() {
     $("#user_fave_color").change(function(){
       var choice = $(this).find("option:selected").val();
       if (choice == "Other") {
-        this.remove();
-        $(".user_fave_color").html(otherFaveColorInput());
-      }
+        replaceSelectFaveColorWithTextInput(this);
+      };
     });
   }
 
-  function otherFaveColorInput() {
-    return "<label>Input your favorite color</label><input type='text' name='user[fave_color]' id='user_fave_color'><br>"
+  // remove select input and replace it with text input
+  function replaceSelectFaveColorWithTextInput(selectForm) {
+    $(selectForm).remove();
+    $(".user_fave_color").append(faveColorTextInputHtml());
   }
+
+  function faveColorTextInputHtml() {
+    return "<input type='text' name='user[fave_color]' id='user_fave_color' class='form-control'>"
+  }
+
   bindFaveColorOtherSelection();
 });
