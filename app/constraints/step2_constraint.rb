edb7ce24fad671688ec0639bc24eadf0765eba71
Nataliya Seryakova <nataliya.seryakova@gmail.com>
2018-02-03 18:30:09 -0500

    get rid of redundant constraint classes

--- app/constraints/step2_constraint.rb
+++
@@ -1,5 +0,0 @@
-class Step2Constraint
-  def matches?(request)
-    request.session["step_url"] == "/onboarding/step2"
-  end
-end
--- app/constraints/step4_constraint.rb
+++
@@ -1,5 +0,0 @@
-class Step4Constraint
-  def matches?(request)
-    request.session["step_url"] == "/onboarding/step4"
-  end
-end
